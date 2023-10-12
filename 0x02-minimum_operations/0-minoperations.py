01b48b6d862ec6f41000342d69537ef279357931
97Fangz <briansegem25@gmail.com>
2023-10-12 03:13:37 -0700

    0x02-minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,34 +10,15 @@ def minOperations(n):
     calculates the fewest number of operations
     needed to result in exactly n H characters in the file.
     """
-    if n <= 1:
+    if not isinstance(n, int):
         return 0
 
-    operations = 0
-    clipboard = 1
-
-    while n > 1:
-        if n % clipboard == 0:
-            operations += 2
-            n //= clipboard
-        else:
-            clipboard += 1
-            operations += 1
-
-    return operations
-
-
-if __name__ == '__main__':
-    from random import randint
-    from time import time
-
-    start_time = time()
-
-    for _ in range(10):
-        n = randint(2, 100)
-        print("Min # of operations to reach {} char (original): {}".
-              format(n, minOperations(n)))
-        print("Min # of operations to reach {} char (modified): {}".
-              format(n, minOperationsModified(n)))
-
-    print(f'==> Program completed in {time() - start_time:.3f}s')
+    op = 0
+    i = 2
+    while (i <= n):
+        if not (n % i):
+            n = int(n / i)
+            op += i
+            i = 1
+        i += 1
+    return op
